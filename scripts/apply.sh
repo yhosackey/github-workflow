81d3083722bf668df44aaee228ed031848d41bd7
Yhoada <yhosackey@gmail.com>
2023-06-09 19:07:45 -0500

    added new tf files

---
+++ scripts/apply.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+terraform init
+terraform fmt
+terraform validate
+terraform plan
+terraform apply
+
---
+++ scripts/git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git status
+git add .
+git status 
+git commit -m "added main.tf file"
+git push
